c32c912ea62eae6aa7a95c44dcbc3967f642e4f6
Hackbright Student <info@hackbrightacademy.com>
2016-04-14 17:48:53 -0700

    melon superclass created and working

--- melons.py
+++ melons.py
@@ -5,16 +5,13 @@ class AbstractMelonOrder(object):
     def __init__(self, species, qty):
         self.species = species
         self.qty = qty
-
-class DomesticMelonOrder(AbstractMelonOrder):
-    """A domestic (in the US) melon order."""
-
-    """Initialize melon order attributes"""
-    def __init__(self, species, qty):
-        super(DomesticMelonOrder, self).__init__(species, qty)
-        self.order_type = "domestic"
-        self.tax = 0.08
         self.shipped = False
+
+    def mark_shipped(self):
+        """Set shipped to true."""
+
+        self.shipped = True
+        print "shipped"
 
     def get_total(self):
         """Calculate price."""
@@ -23,36 +20,27 @@ class DomesticMelonOrder(AbstractMelonOrder):
         total = (1 + self.tax) * self.qty * base_price
         return total
 
-    def mark_shipped(self):
-        """Set shipped to true."""
+class DomesticMelonOrder(AbstractMelonOrder):
+    """A domestic (in the US) melon order."""
 
-        self.shipped = True
+    def __init__(self, species, qty):
+        super(DomesticMelonOrder, self).__init__(species, qty)
+        self.order_type = "domestic"
+        self.tax = 0.08
 
 
 class InternationalMelonOrder(AbstractMelonOrder):
     """An international (non-US) melon order."""
 
     def __init__(self, species, qty, country_code):
-        """Initialize melon order attributes"""
+        super(InternationalMelonOrder, self).__init__(species, qty)
 
         self.country_code = country_code
-        self.shipped = False
         self.order_type = "international"
         self.tax = 0.17
-
-    def get_total(self):
-        """Calculate price."""
-
-        base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
-        return total
-
-    def mark_shipped(self):
-        """Set shipped to true."""
-
-        self.shipped = True
-
+  
     def get_country_code(self):
         """Return the country code."""
 
         return self.country_code
+
